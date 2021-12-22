2b2e3b62a5cb0f9d1a298636cb1c6c5d20adeb9f
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2021-12-22 01:06:09 +0100

    commit

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -5,37 +5,74 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class SiteController extends AbstractController
 {
     /**
-     *Route("/", name: "Accueil")
+     * @Route("/", name="test")
      */
-    public function index(): Response
+    public function accueilNoLocale(): Response
     {
-        return $this->render('site/index.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->redirectToRoute("accueil");
+    }
+    /**
+     * @Route("/portfolio")
+     */
+    public function portfolioNoLocale(): Response
+    {
+        return $this->redirectToRoute("portfolio");
+    }
+    /**
+     * @Route("/{{ request.getLocale() }}/", name="accueil")
+     */
+    public function acceuil(Request $request): Response
+    {
+        $lang = $request->getLocale();
+        if ($lang == 'fr'){
+            return $this->render('site/fr/index.html.twig',[
+                'controller_name'=>'test'
+            ]);
+        }
+        if ($lang == 'en'){
+            return $this->render('site/fr/index.html.twig',[
+                'controller_name'=>$lang
+            ]);
+        }
+        else{
+            return $this->render('site/langnotfound.html.twig',[
+                'controller_name'=>$lang
+            ]);
+        }
     }
 	/**
-     * @Route("/portfolio", name="Portfolio")
+     * @Route("/{_locale}/portfolio", name="portfolio")
      */
-	public function portfolio()
+	public function portfolio(Request $request): Response
     {
-        return $this->render('site/portfolio.html.twig');
+        $lang = $request->getLocale();
+        if ($lang == 'fr'){
+            return $this->render('site/fr/portfolio.html.twig');
+        }
     }
 	/**
-     * @Route("/cv", name="cv")
+     * @Route("/{_locale}/cv", name="cv")
      */
-	public function cv()
+	public function cv(Request $request): Response
     {
-        return $this->render('site/cv.html.twig');
+        $lang = $request->getLocale();
+        if ($lang == 'fr'){
+            return $this->render('site/cv.html.twig');
+        }
     }
 	/**
-     * @Route("/blog", name="blog")
+     * @Route("/{_locale}/blog", name="blog")
      */
-	public function blog()
+	public function blog(Request $request): Response
     {
-        return $this->render('site/blog.html.twig');
+        $lang = $request->getLocale();
+        if ($lang == 'fr'){
+            return $this->render('site/blog.html.twig');
+        }
     }
 }
